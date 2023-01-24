55c7da504c571bfe0d188f0c99726d3d85cc57d9
syndelldev <priyankap.syndell@gmail.com>
2023-01-24 16:58:38 +0530

    added css

--- src/Components/Charts/SankeyChart/RND.js
+++ src/Components/Charts/SankeyChart/RND.js
@@ -33,9 +33,8 @@ const RND = props => {
         <div className="App">
           <div className="sankeys">
             <img className="image" src={RNDIMAGE} useMap="#planetmap" />
-            <map id="planetmap" name="planetmap">
+            {/* <map id="planetmap" name="planetmap">
                 <area className="button" onClick={() => imageClick1()} alt="ORGANIC" title= {data && data[0].ORGANIC}  coords="26,220,7" shape="circle" /> 
-
                 <area className="button" onClick={() => imageClick2()} target="" alt="PAID" title="PAID" coords="26,516,8" shape="circle" />
                 <area target="" alt="RETURNING" title="RETURNING" coords="185,272,6" shape="circle" />
                 <area target="" alt="NEW" title="NEW" coords="183,480,6" shape="circle" />
@@ -47,24 +46,68 @@ const RND = props => {
                 <area target="" alt="WATCH ADS" title="WATCH ADS" coords="699,447,7" shape="circle" />
                 <area target="" alt="WATCH PREMIUM CONTENT" title="WATCH PREMIUM CONTENT"  coords="898,339,8" shape="circle" />
                 <area target="" alt="WATCH FREE CONTENT" title="WATCH FREE CONTENT"  coords="898,398,5" shape="circle" />
-            </map>
+            </map> */}
+
+            {data && <div className="org text">
+              <label> {data[0].ORGANIC} </label>
+            </div>}
+
+            {data && <div className="new text">
+              <label> {data[0].NEW} </label>
+            </div>}
+
+            {data && <div className="paid text">
+              <label> {data[0].PAID} </label>
+            </div>}
+
+            {data && <div className="ret text">
+              <label> {data[0].RETURNING} </label>
+            </div>}
+
+            {data && <div className="ano text">
+              <label> {data[0].ANONYMOUS} </label>
+            </div>}
+
+
+            {data && <div className="sin text">
+              <label> {data[0].SIGNUP} </label>
+            </div>}
+
+            
+            {data && <div className="exp text">
+              <label> {data[0].EXPLORE} </label>
+            </div>}
+
+            
+            {data && <div className="search text">
+              <label> {data[0].SEARCH} </label>
+            </div>}
+
+            
+            {data && <div className="sub text">
+              <label> {data[0].SUBSCRIBED} </label>
+            </div>}
+
+            
+            {data && <div className="wa text">
+              <label> {data[0].WATCHADS} </label>
+            </div>}
+
+            
+            {data && <div className="wpc text">
+              <label> {data[0].WATCHPREMIUMCONTENT} </label>
+            </div>}
+
+            {data && <div className="wfc text">
+              <label> {data[0].WATCHFREECONTENT} </label>
+            </div>}
+
+
+
+
+
           </div>
 
-          {data && <div className="org">
-            <label> {data[0].ORGANIC} </label>
-          </div>}
-
-          {data && <div className="paid">
-            <label> {data[0].PAID} </label>
-          </div>}
-
-          {data && <div className="ret">
-            <label> {data[0].RETURNING} </label>
-          </div>}
-
-          {data && <div className="new">
-            <label> {data[0].NEW} </label>
-          </div>}
 
 
         <div className="footer ">
