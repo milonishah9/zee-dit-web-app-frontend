45cd9d4c1b4795551705513550a710a58ccbda9a
syndelldev <priyankap.syndell@gmail.com>
2023-01-30 10:32:00 +0530

    getBBox

--- src/Components/Charts/DivergingSB/DivergingSB.js
+++ src/Components/Charts/DivergingSB/DivergingSB.js
@@ -6,38 +6,40 @@ import './SB.css'
 
 const DivergingSB = () => {
 
-    var margin = {top: 50, right: 20, bottom: 10, left: 65},
-        width = 400 - margin.left - margin.right,
-        height = 250 - margin.top - margin.bottom;
+        
+        var margin = {top: 50, right: 20, bottom: 10, left: 65},
+        width = 800 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
 
-    var y = d3.scale.ordinal()
+        var y = d3.scale.ordinal()
         .rangeRoundBands([0, height], .3);
 
-    var x = d3.scale.linear()
+        var x = d3.scale.linear()
         .rangeRound([0, width]);
 
-    var color = d3.scale.ordinal()
+        var color = d3.scale.ordinal()
         .range(["#c7001e", "#f6a580", "#cccccc", "#92c6db", "#086fad"]);
 
-    var xAxis = d3.svg.axis()
+        var xAxis = d3.svg.axis()
         .scale(x)
         .orient("top");
 
-    var yAxis = d3.svg.axis()
+        var yAxis = d3.svg.axis()
         .scale(y)
         .orient("left")
 
-    var svg = d3.select("#figure").append("svg")
+        var svg = d3.select("#figure").append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .attr("id", "d3-plot")
-    .append("g")
+        .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+        color.domain(["Strongly disagree", "Disagree", "Neither agree nor disagree", "Agree", "Strongly agree"]);
 
-    d3.csv(csv, function(error, data) {
+        d3.csv(csv, function(error, data) {
 
-    data.forEach(function(d) {
+        data.forEach(function(d) {
         // calc percentages
         d["Strongly disagree"] = +d[1]*100/d.N;
         d["Disagree"] = +d[2]*100/d.N;
@@ -47,44 +49,44 @@ const DivergingSB = () => {
         var x0 = -1*(d["Neither agree nor disagree"]/2+d["Disagree"]+d["Strongly disagree"]);
         var idx = 0;
         d.boxes = color.domain().map(function(name) { return {name: name, x0: x0, x1: x0 += +d[name], N: +d.N, n: +d[idx += 1]}; });
-    });
+        });
 
-    var min_val = d3.min(data, function(d) {
+        var min_val = d3.min(data, function(d) {
             return d.boxes["0"].x0;
             });
 
-    var max_val = d3.max(data, function(d) {
+        var max_val = d3.max(data, function(d) {
             return d.boxes["4"].x1;
             });
 
-    x.domain([min_val, max_val]).nice();
-    y.domain(data.map(function(d) { return d.Question; }));
+        x.domain([min_val, max_val]).nice();
+        y.domain(data.map(function(d) { return d.Question; }));
 
-    // svg.append("g")
-    //     .attr("class", "x axis")
-    //     .call(xAxis);
+        svg.append("g")
+        .attr("class", "x axis")
+        .call(xAxis);
 
-    // svg.append("g")
-    //     .attr("class", "y axis")
-    //     .call(yAxis)
+        svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
 
-    var vakken = svg.selectAll(".question")
+        var vakken = svg.selectAll(".question")
         .data(data)
         .enter().append("g")
         .attr("class", "bar")
         .attr("transform", function(d) { return "translate(0," + y(d.Question) + ")"; });
 
-    var bars = vakken.selectAll("rect")
+        var bars = vakken.selectAll("rect")
         .data(function(d) { return d.boxes; })
         .enter().append("g").attr("class", "subbar");
 
-    bars.append("rect")
+        bars.append("rect")
         .attr("height", y.rangeBand())
         .attr("x", function(d) { return x(d.x0); })
         .attr("width", function(d) { return x(d.x1) - x(d.x0); })
         .style("fill", function(d) { return color(d.name); });
 
-    bars.append("text")
+        bars.append("text")
         .attr("x", function(d) { return x(d.x0); })
         .attr("y", y.rangeBand()/2)
         .attr("dy", "0.5em")
@@ -93,7 +95,7 @@ const DivergingSB = () => {
         .style("text-anchor", "begin")
         .text(function(d) { return d.n !== 0 && (d.x1-d.x0)>3 ? d.n : "" });
 
-    vakken.insert("rect",":first-child")
+        vakken.insert("rect",":first-child")
         .attr("height", y.rangeBand())
         .attr("x", "1")
         .attr("width", width)
@@ -101,29 +103,29 @@ const DivergingSB = () => {
         .style("fill", "#F5F5F5")
         .attr("class", function(d,index) { return index%2==0 ? "even" : "uneven"; });
 
-    svg.append("g")
+        svg.append("g")
         .attr("class", "y axis")
-    .append("line")
+        .append("line")
         .attr("x1", x(0))
         .attr("x2", x(0))
         .attr("y2", height);
 
-    var startp = svg.append("g").attr("class", "legendbox").attr("id", "mylegendbox");
-    // this is not nice, we should calculate the bounding box and use that
-    var legend_tabs = [0, 120, 200, 375, 450];
-    var legend = startp.selectAll(".legend")
+        var startp = svg.append("g").attr("class", "legendbox").attr("id", "mylegendbox");
+        // this is not nice, we should calculate the bounding box and use that
+        var legend_tabs = [0, 120, 200, 375, 450];
+        var legend = startp.selectAll(".legend")
         .data(color.domain().slice())
         .enter().append("g")
         .attr("class", "legend")
         .attr("transform", function(d, i) { return "translate(" + legend_tabs[i] + ",-45)"; });
 
-    legend.append("rect")
+        legend.append("rect")
         .attr("x", 0)
         .attr("width", 18)
         .attr("height", 18)
         .style("fill", color);
 
-    legend.append("text")
+        legend.append("text")
         .attr("x", 22)
         .attr("y", 9)
         .attr("dy", ".35em")
@@ -131,19 +133,18 @@ const DivergingSB = () => {
         .style("font" ,"10px sans-serif")
         .text(function(d) { return d; });
 
-    d3.selectAll(".axis path")
+        d3.selectAll(".axis path")
         .style("fill", "none")
         .style("stroke", "#000")
         .style("shape-rendering", "crispEdges")
 
-    d3.selectAll(".axis line")
+        d3.selectAll(".axis line")
         .style("fill", "none")
         .style("stroke", "#000")
         .style("shape-rendering", "crispEdges")
 
-        
-    // var movesize = width/2 ;
-    // d3.selectAll(".legendbox").attr("transform", "translate(" + movesize  + ",0)");
+        var movesize = width/2 - startp.node().getBBox().width/2;
+        d3.selectAll(".legendbox").attr("transform", "translate(" + movesize  + ",0)");
 
 
     });
