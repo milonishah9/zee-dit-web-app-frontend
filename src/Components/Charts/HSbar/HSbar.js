01f008a4471915d7cdf45913b798788d56ff19ec
syndelldev <106512706+priyankap-bit@users.noreply.github.com>
2023-01-30 09:52:25 +0530

    Update HSbar.js

--- src/Components/Charts/HSbar/HSbar.js
+++ src/Components/Charts/HSbar/HSbar.js
@@ -5,7 +5,6 @@ import "./HSbar.css";
 
 
 const HSbar = (props) => {
-<<<<<<< Updated upstream
   var {data,totalValueArr} = props;
   let total = 0;
   let totalValue = []
@@ -15,21 +14,14 @@ const HSbar = (props) => {
   //   return total += element.value;
   //   // totalValue.push(total)
   // }));
-=======
   var {data} = props;
   
->>>>>>> Stashed changes
   // let total = 0;
   // data.map((element, index) => {
   //   total += element.value
   // })
-<<<<<<< Updated upstream
   // console.log(totalValue);
   // console.log(data);
-=======
-  console.log(data);
-  
->>>>>>> Stashed changes
     return (
       <div className="hs-bar">
         <HSBar
@@ -37,10 +29,7 @@ const HSbar = (props) => {
           data={data}
           className="hs-bar"
         />
-<<<<<<< Updated upstream
         <span className="ttlval">{totalValueArr}</span>
-=======
->>>>>>> Stashed changes
       </div>
     );
   }
