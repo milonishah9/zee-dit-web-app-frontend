81df8c42a5b719ac75407fa60aeb4bf60c6910c8
syndelldev <priyankap.syndell@gmail.com>
2023-01-31 12:44:22 +0530

    updated product screen

--- src/Pages/ProductPage/Product.js
+++ src/Pages/ProductPage/Product.js
@@ -36,11 +36,12 @@ const Product = () => {
                 <div className={classed}>
                   <h2>322.6 Minutes</h2>
                   <p>6 % from last period</p>
+                  <div className="product-icons">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    width="26"
-                    height="26"
-                    fill="currentColor"
+                    width="36"
+                    height="36"
+                    fill="#37b363"
                     class="bi bi-android2"
                     viewBox="0 0 16 16"
                   >
@@ -48,9 +49,9 @@ const Product = () => {
                   </svg>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    width="26"
-                    height="26"
-                    fill="currentColor"
+                    width="36"
+                    height="36"
+                    fill="gray"
                     class="bi bi-apple "
                     viewBox="0 0 16 16"
                   >
@@ -59,15 +60,16 @@ const Product = () => {
                   </svg>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    width="26"
-                    height="26"
-                    fill="currentColor"
+                    width="36"
+                    height="36"
+                    fill="#328ee9"
                     class="bi bi-calendar-event"
                     viewBox="0 0 16 16"
                   >
                     <path d="M11 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1z" />
                     <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
                   </svg>
+                  </div>
                 </div>
 
                 <div className="my-pluse-minus-buttons">
