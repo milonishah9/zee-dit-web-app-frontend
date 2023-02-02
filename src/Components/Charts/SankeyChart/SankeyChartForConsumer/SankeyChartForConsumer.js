533d6757cb98c3452f196c3bd83d8822d2949fc2
Miloni Shah <vc.miloni.shah@zee.com>
2023-02-02 12:01:04 +0530

    Added responsivity to consumer journey

--- src/Components/Charts/SankeyChart/SankeyChartForConsumer/SankeyChartForConsumer.js
+++ src/Components/Charts/SankeyChart/SankeyChartForConsumer/SankeyChartForConsumer.js
@@ -31,15 +31,15 @@ const SankeyChartForConsumer = (props) => {
             { id: "Sign up", x: xPosition + 125, y: yPosition - 15, value: 33.21, name: "Sign Up", absoluteValue: 66.3, percentageValue: 18, percentageChage: 7 },
             { id: "Explore", x: xPosition - 35, y: yPosition + 205, value: 86.71, name: "Explore", absoluteValue: 66.3, percentageValue: 50, percentageChage: 5 },
             { id: "Search", x: xPosition + 55, y: yPosition + 205, value: 33.21, name: "Search", absoluteValue: 66.3, percentageValue: 20, percentageChage: -20 },
-            { id: "Exit-signup", x: xPosition + 125, y: yPosition + 205, value: 5.5, name: "Exit", absoluteValue: 66.3, percentageValue: 27, percentageChage: 13 },
-            { id: "Exit-anonymous", x: xPosition - 100, y: yPosition + 205, value: 5.5, name: "Exit", absoluteValue: 66.3, percentageValue: 3, percentageChage: -0 },
+            { id: "Exit-signup", x: xPosition + 125, y: yPosition + 205, value: 5.55, name: "Exit", absoluteValue: 66.3, percentageValue: 27, percentageChage: 13 },
+            { id: "Exit-anonymous", x: xPosition - 100, y: yPosition + 205, value: 5.55, name: "Exit", absoluteValue: 66.3, percentageValue: 3, percentageChage: -0 },
             { id: "Subscribed", x: xPosition - 25, y: yPosition + 405, value: 86.71, name: "Subscribed", absoluteValue: 54, percentageValue: 7, percentageChage: -2 },
             { id: "Watched Ads", x: xPosition + 50, y: yPosition + 405, value: 33.21, name: "Watched Ads", absoluteValue: 66.3, percentageValue: 30, percentageChage: -2 },
             { id: "Exit-Search", x: xPosition + 125, y: yPosition + 405, value: 33.21, name: "Exit", absoluteValue: 66.3, percentageValue: 12, percentageChage: -2 },
             { id: "Exit-Explore", x: xPosition - 100, y: yPosition + 405, value: 33.21, name: "Exit", absoluteValue: 66.3, percentageValue: 51, percentageChage: -2 },
-            { id: "Watch Premium Content", x: xPosition - 15, y: yPosition + 575, value: 86.71, name: "Watch Premium Content", absoluteValue: 66.3, percentageValue: 3, percentageChage: -2 },
-            { id: "Watch Free Content", x: xPosition + 35, y: yPosition + 575, value: 86.71, name: "Watch Free Content", absoluteValue: 66.3, percentageValue: 54, percentageChage: -2 },
-            { id: "Exit - Watch Ads", x: xPosition + 100, y: yPosition + 575, value: 86.71, name: "Exit", absoluteValue: 66.3, percentageValue: 43, percentageChage: -2 },
+            { id: "Watch Premium Content", x: xPosition - 25, y: yPosition + 575, value: 86.71, name: "Watch Premium Content", absoluteValue: 66.3, percentageValue: 3, percentageChage: -2 },
+            { id: "Watch Free Content", x: xPosition + 50, y: yPosition + 575, value: 86.71, name: "Watch Free Content", absoluteValue: 66.3, percentageValue: 54, percentageChage: -2 },
+            { id: "Exit - Watch Ads", x: xPosition + 125, y: yPosition + 575, value: 86.71, name: "Exit", absoluteValue: 66.3, percentageValue: 43, percentageChage: -2 },
 
             // { id: "Organic", x: 25, y: 25, value: 70.38, name: "Organic", absoluteValue: 66.3, percentageValue: 46, percentageChage: -2 },
             // { id: "Paid", x: 250, y: 25, value: 79.9, name: "Paid", absoluteValue: 77.9, percentageValue: 54, percentageChage: 6 },
@@ -63,7 +63,7 @@ const SankeyChartForConsumer = (props) => {
         var linkData = [
             { source: [xPosition - 100, yPosition - 365], target: [xPosition + 125, yPosition - 205], fill: "#74CDFF" }, //Organic - New
             { source: [xPosition - 100, yPosition - 365], target: [xPosition - 100, yPosition - 205], fill: "#74CDFF" }, //Orgainc - Returning
-            { source: [xPosition + 125, yPosition - 365], target: [xPosition - 100, yPosition - 205], fill: "#768CFF" }, //Paid - Returning
+            // { source: [xPosition + 125, yPosition - 365], target: [xPosition - 100, yPosition - 205], fill: "#768CFF" }, //Paid - Returning
             { source: [xPosition + 125, yPosition - 365], target: [xPosition + 125, yPosition - 205], fill: "#768CFF" }, //Paid - New
             { source: [xPosition - 100, yPosition - 185], target: [xPosition - 100, yPosition - 25], fill: "#FFB78E" },  //Returning - Anonymous
             { source: [xPosition - 100, yPosition - 185], target: [xPosition + 125, yPosition - 25], fill: "#FFB78E" }, //Returning - Sign up
@@ -83,10 +83,10 @@ const SankeyChartForConsumer = (props) => {
             { source: [xPosition + 55, yPosition + 215], target: [xPosition - 25, yPosition + 395], fill: "#FFB78E" }, //Search - Subscribed
             { source: [xPosition + 55, yPosition + 215], target: [xPosition + 50, yPosition + 395], fill: "#FFB78E" }, //Search - WatchAds
             { source: [xPosition + 55, yPosition + 215], target: [xPosition + 125, yPosition + 395], fill: "#CAB6B6" }, //Explore - WatchAds
-            { source: [xPosition - 25, yPosition + 415], target: [xPosition - 15, yPosition + 565], fill: "#FFB78E" }, //Subscribed - Watch Premium Content
-            { source: [xPosition - 25, yPosition + 415], target: [xPosition + 35, yPosition + 565,], fill: "#FFB78E" }, //Subscribed - Watch Free Content
-            { source: [xPosition + 50, yPosition + 415], target: [xPosition + 35, yPosition + 565,], fill: "#768CFF" }, //Explore - WatchAds
-            { source: [xPosition + 50, yPosition + 415], target: [xPosition + 100, yPosition + 565], fill: "#CAB6B6" }, //Explore - Exit Watch Ads
+            { source: [xPosition - 25, yPosition + 415], target: [xPosition - 25, yPosition + 565], fill: "#FFB78E" }, //Subscribed - Watch Premium Content
+            { source: [xPosition - 25, yPosition + 415], target: [xPosition + 50, yPosition + 565,], fill: "#FFB78E" }, //Subscribed - Watch Free Content
+            { source: [xPosition + 50, yPosition + 415], target: [xPosition + 50, yPosition + 565,], fill: "#768CFF" }, //Explore - WatchAds
+            { source: [xPosition + 50, yPosition + 415], target: [xPosition + 125, yPosition + 565], fill: "#CAB6B6" }, //Explore - Exit Watch Ads
 
             // { source: [25, 35], target: [195, 215], fill: "#74CDFF" }, //Organic - New
             // { source: [25, 35], target: [70, 215], fill: "#74CDFF" }, //Orgainc - Returning
@@ -133,7 +133,7 @@ const SankeyChartForConsumer = (props) => {
 
         // const points = [[250, 35], [200, 50], [150, 90], [200, 75], [200, 75], [150, 80], [100, 125], [250,35]];
         // const points = [[xPosition + 125, yPosition - 365], [xPosition - 100, yPosition - 205]];
-        // [[35, 250], [110, 200], [130, 90], [190, 30], [178, 35], [120, 80], [90, 200], [35, 250]]
+        const points = [[35, 250], [110, 200], [130, 90], [190, 30], [178, 35], [120, 80], [90, 200], [35, 250]]
         const svg = d3
             .select(svgRef.current)
             .attr("viewBox", "0 0 1030 300")
@@ -186,7 +186,7 @@ const SankeyChartForConsumer = (props) => {
             // .attr("x", d => d.y)
             // .attr("y", d => d.x + 15)
             .attr("x", d => d.name === "Exit" ? d.y + 3 : d.y)
-            .attr("y", d => d.name === "Exit" ? d.x + 2 : d.x + 15)
+            .attr("y", d => d.name === "Exit" ? d.x + 2 : d.x + 17)
             .classed("node-title", true)
             .text(d => d.name.toUpperCase());
 
@@ -198,7 +198,7 @@ const SankeyChartForConsumer = (props) => {
             // .attr("x", d => d.y)
             // .attr("y", d => d.x + 28)
             .attr("x", d => d.name === "Exit" ? d.y + 3 : d.y)
-            .attr("y", d => d.name === "Exit" ? d.x + 18 : d.x + 30)
+            .attr("y", d => d.name === "Exit" ? d.x + 22 : d.x + 34)
             .classed("node-value", true)
             .text(d => d.value);
 
@@ -210,7 +210,7 @@ const SankeyChartForConsumer = (props) => {
             // .attr("x", d => d.y)
             // .attr("y", d => d.x + 38)
             .attr("x", d => d.name === "Exit" ? d.y + 3 : d.y)
-            .attr("y", d => d.name === "Exit" ? d.x + 28 : d.x + 40)
+            .attr("y", d => d.name === "Exit" ? d.x + 33 : d.x + 46)
             .classed("node-percentage", true)
             .text(d => `(${d.percentageValue}%)`);
 
@@ -221,23 +221,23 @@ const SankeyChartForConsumer = (props) => {
             .attr("text-anchor", "middle")
             // .attr("x", d => d.y + 24)
             // .attr("y", d => d.x + 26)
-            .attr("x", d => d.name === "Exit" ? d.y + 28 : d.y + 28)
-            .attr("y", d => d.name === "Exit" ? d.x + 12 : d.x + 26)
+            .attr("x", d => d.name === "Exit" ? d.y + 33 : d.y + 33)
+            .attr("y", d => d.name === "Exit" ? d.x + 16 : d.x + 30)
             // .attr("color", "red")
             .attr("class", d => d.percentageChage >= 0 ? "node-percentage-change-pos" : "node-percentage-change-neg")
             // .classed("node-percentage-change", true)
             .text(d => `${Math.abs(d.percentageChage)}%`);
 
-        // svg
-        //     .append('path')
-        //     // .data(coOrdinates)
-        //     .attr('d', curve(point))
-        //         // .attr('stroke', 'black')
-        //         .attr("opacity", "0.5")
-        //         // with multiple points defined, if you leave out fill:none,
-        //         // the overlapping space defined by the points is filled with
-        //         // the default value of 'black'
-        //         .attr('fill', '#768CFF');
+        svg
+            .append('path')
+            // .data(coOrdinates)
+            .attr('d', curve(points))
+                // .attr('stroke', 'black')
+                .attr("opacity", "0.5")
+                // with multiple points defined, if you leave out fill:none,
+                // the overlapping space defined by the points is filled with
+                // the default value of 'black'
+                .attr('fill', '#768CFF');
 
         // svg
         // .append("p")
