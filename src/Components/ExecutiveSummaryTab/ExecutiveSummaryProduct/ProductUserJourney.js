aec178adc49440797b16fcea9f87bc6515eaedfd
priyanka <vc.miloni.shah@zee.com>
2023-02-02 12:34:38 +0530

    experience changed

--- src/Components/ExecutiveSummaryTab/ExecutiveSummaryProduct/ProductUserJourney.js
+++ src/Components/ExecutiveSummaryTab/ExecutiveSummaryProduct/ProductUserJourney.js
@@ -21,8 +21,8 @@ const ProductUserJourney = (props) => {
           <div className="product-icons-logo">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="36"
-            height="36"
+            width="25"
+            height="25"
             fill="#37b363"
             class="bi bi-android2"
             viewBox="0 0 16 16"
@@ -34,8 +34,8 @@ const ProductUserJourney = (props) => {
           <div className="product-icons-logo">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="36"
-            height="36"
+            width="25"
+            height="25"
             fill="gray"
             class="bi bi-apple "
             viewBox="0 0 16 16"
@@ -48,8 +48,36 @@ const ProductUserJourney = (props) => {
           <div className="product-icons-logo">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="36"
-            height="36"
+            width="25"
+            height="25"
+            fill="#328ee9"
+            class="bi bi-calendar-event"
+            viewBox="0 0 16 16"
+          >
+            <path d="M11 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1z" />
+            <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
+          </svg>
+          <p>30%</p>
+          </div>
+          <div className="product-icons-logo">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="25"
+            height="25"
+            fill="#328ee9"
+            class="bi bi-calendar-event"
+            viewBox="0 0 16 16"
+          >
+            <path d="M11 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1z" />
+            <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
+          </svg>
+          <p>30%</p>
+          </div>
+          <div className="product-icons-logo">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="25"
+            height="25"
             fill="#328ee9"
             class="bi bi-calendar-event"
             viewBox="0 0 16 16"
