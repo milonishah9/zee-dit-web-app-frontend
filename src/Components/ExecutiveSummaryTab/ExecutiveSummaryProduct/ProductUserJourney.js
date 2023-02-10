08c986ad0199c84da73dff75507f8cfea0c09967
syndelldev <106512706+priyankap-bit@users.noreply.github.com>
2023-02-10 12:43:56 +0530

    product changes applied

--- src/Components/ExecutiveSummaryTab/ExecutiveSummaryProduct/ProductUserJourney.js
+++ src/Components/ExecutiveSummaryTab/ExecutiveSummaryProduct/ProductUserJourney.js
@@ -16,7 +16,15 @@ const ProductUserJourney = (props) => {
 
   return (
     <div className="product-userjourney-card">
-      <p className="product-userjourney-card-heading">{data[0]}</p>
+      <div className="product-userjourney-card-heading-and-icon">
+        <p className="product-userjourney-card-heading">{data[0]}</p>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
+          <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+          <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+        </svg>
+
+      </div>
+      
       <p className="product-userjourney-card-number">{data[1]}</p>
       <p className={data[2] > 0 ? ('pluse-value'):('minuse-value') }>{data[2] > 0 ? (data[2]+'% ↑'):(data[2]+'% ↓')}</p>
       <div className={classed}>
